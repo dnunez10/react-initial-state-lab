f071e72934aa575e13281ebcca9f61b6a05ee0a0
dnunez10 <diana_nunez10@yahoo.com>
2021-04-27 11:28:22 -0700

    commit tree 0660f757c7659861319e2dba6f5d2ec91137f4b8

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,33 @@
 // your Bomb code here!
+import React from 'react'
+
+class Bomb extends React.Component {
+
+    constructor(props){
+        super()
+        this.state = {
+        secondsLeft: props.initialCount
+    }
+    }
+
+    decrease = () => {
+        const newTime = this.state.secondsLeft - 1
+        this.setState({
+            secondsLeft: newTime
+        })
+    }
+
+    render() {        
+        return (
+            <div onClick={this.decrease}>
+                <h1>
+                {this.state.secondsLeft > 0 ? this.state.secondsLeft + ' seconds left before I go boom!' : 'Boom!'}
+                </h1>
+            </div>
+            
+        )
+}
+}
+
+
+export default Bomb
